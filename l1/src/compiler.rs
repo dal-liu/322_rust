8b3116b85f858cb943efad4ea7a3d616e7f30387
Daniel Liu <daniel.albert.liu@gmail.com>
2025-10-21 23:20:19 -0400

    remove clap dependency

--- l1/src/compiler.rs
+++ l1/src/compiler.rs
@@ -2,17 +2,12 @@ mod codegen;
 mod l1;
 mod parser;
 
-use clap::Parser;
 use codegen::generate_code;
 use parser::parse_file;
-
-#[derive(Parser)]
-struct Cli {
-    source: String,
-}
+use std::env;
 
 fn main() {
-    let cli = Cli::parse();
-    let program = parse_file(&cli.source).unwrap();
+    let args: Vec<String> = env::args().collect();
+    let program = parse_file(&args[1]).unwrap();
     generate_code(&program).unwrap();
 }
